aa9b6bb81fc348c1a97b699ff36366fe5a8f4b3f
MINSEOK LEE <lms31782@gmail.com>
2023-05-18 10:39:46 +0900

    테마추가, Prettie수정

---
+++ src/config/theme.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#3da1ff',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Noto Sans KR',
+      'sans-serif',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Fira Sans',
+      'Droid Sans',
+      'Helvetica Neue',
+    ].join(','),
+  },
+});
+
+export function CustomThemeProvider({ children }: { children: React.ReactNode }) {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import { AuthProvider } from '@context/authContext';
-import { BrowserRouter } from 'react-router-dom';
+import { CustomThemeProvider } from '@config/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <AuthProvider>
-      <CssBaseline />
-      <App />
-    </AuthProvider>
+    <CustomThemeProvider>
+      <AuthProvider>
+        <CssBaseline />
+        <App />
+      </AuthProvider>
+    </CustomThemeProvider>
   </BrowserRouter>
   // </React.StrictMode>
 );
